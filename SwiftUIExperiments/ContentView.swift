c5f93986ccc7ba1fe667ba8f8057867d12d9171d
Dominik Hauser <dom@dasdom.de>
2019-06-15 21:35:56 +0200

    Initial Commit

---
+++ SwiftUIExperiments/ContentView.swift
@@ -0,0 +1,19 @@
+//  Created by dasdom on 15.06.19.
+//  Copyright © 2019 dasdom. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
